b46e4134b3d655c917ef52d59fbc2c1196700bf4
raphsutti <neoster9@gmail.com>
2018-11-30 19:49:26 +1100

    initial commit

---
+++ base64decode1.py
@@ -0,0 +1,17 @@
+import binascii
+# decode U2FtcGxlIHRleHQ=
+# decode VGhpcyBpcyB0b28gZWFzeQ==
+
+# decode (encoded multiple times) VWtkc2EwbEliSFprVTBJeFl6SlZaMWxUUW5OaU1qbDNVSGM5UFFvPQo=
+
+text = raw_input("Enter text: ")
+loopTimes = int(raw_input("Enter loop amount: "))
+
+for i in range(loopTimes):
+  try:
+    decodedText = text.decode("base64")
+    print decodedText
+    text = decodedText
+  except binascii.Error as err:
+    print "Incorrect padding error"
+  
