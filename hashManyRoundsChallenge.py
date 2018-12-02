1bca0cffb914cf493c8bea6bad7fc0df9854aff6
raphsutti <neoster9@gmail.com>
2018-12-03 10:36:07 +1100

    Add time taken

--- hashManyRoundsChallenge.py
+++ hashManyRoundsChallenge.py
@@ -1,7 +1,10 @@
 import hashlib
+import time
 # 100 rounds of MD5 aka "stretching"
 # find password that result in hash of c09145ad46b058fba82e4218169c7121
 # password is between 000 and 999
+
+startTime = time.time()
 
 for p1 in "0123456789":
   for p2 in "0123456789":
@@ -13,3 +16,5 @@ for p1 in "0123456789":
       # print password, h
       if h == "c09145ad46b058fba82e4218169c7121":
         print password, h
+
+print "Time taken: %.03f seconds" % (time.time() - startTime)
